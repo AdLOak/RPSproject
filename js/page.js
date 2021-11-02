2713490d786d8fd384a73cadf180cafe7366b0fb
AdLOak <86484895+AdLOak@users.noreply.github.com>
2021-11-02 11:46:42 -0400

    creates javascript play()
     Calculates a random number (0-2)
    Uses if statements to determine the opponents selections
    Adds switch statements

--- js/page.js
+++ js/page.js
@@ -1,5 +1,44 @@
-function play(){
-  var num = Math.floor(Math.rand()*3);
+function play(userSel){
+  var num = Math.floor(Math.random()*3);
+
   document.getElementById("result").innerHTML = num;
+  if(num === 0)
+  {
+    //rock
+    let computerChoice = 'rock';
+    // window.alert(computerChoice);
+    document.getElementById("o-img").innerHTML = computerChoice;
+  }
+  else if (num === 1)
+  {
+    //paper
+    let computerChoice = 'paper';
+    // window.alert(computerChoice);
+    document.getElementById("o-img").innerHTML = computerChoice;
+
+  }
+  else
+  {
+    //scissors
+    let computerChoice = 'scissors';
+    // window.alert(computerChoice);
+    document.getElementById("o-img").innerHTML = computerChoice;
+
+  }
+  switch (userSel) {
+    case 'rock':
+      // window.alert("rock selected");
+
+    break;
+    case 'paper':
+      // window.alert("paper selected");
+    break;
+    case 'scissors':
+      // window.alert("scissors selected");
+    break;
+    default:
+
+  }
+  // window.alert(num);
 
 }
