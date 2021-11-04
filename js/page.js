6e621cbf24625d98aaefb8c09dd00c33ac7a95c6
AdLOak <86484895+AdLOak@users.noreply.github.com>
2021-11-04 12:01:29 -0400

    removes if-else if-else statements to implement a new method of determining outcome

--- js/page.js
+++ js/page.js
@@ -5,67 +5,28 @@ function play(userSel){
   if(num === 0)
   {
     //rock
-    let computerChoice = 'rock';
-    // window.alert(computerChoice);
-
   }
   else if (num === 1)
   {
     //paper
-    let computerChoice = 'paper';
-    // window.alert(computerChoice);
-
   }
   else
   {
     //scissors
-    let computerChoice = 'scissors';
-    // window.alert(computerChoice);
-
   }
   switch (userSel) {
     case 'rock':
-      // window.alert("rock selected");
-
-      if(computerChoice === 'rock')
-      {
-        // document.getElementById("result").innerHTML = "It's a Tie";
-      }
-      else if(computerChoice === 'paper')
-      {
-        // document.getElementById("result").innerHTML = "You Lost";
-      }
-      else
       {
         // document.getElementById("result").innerHTML = "You Won";
       }
     break;
     case 'paper':
       // window.alert("paper selected");
-      if(computerChoice === 'rock')
       {
-          // document.getElementById("result").innerHTML = "You Won";
-      }
-      else if(computerChoice === 'paper')
-      {
-        // document.getElementById("result").innerHTML = "It's a Tie";
-      }
-      else
-      {
-        // document.getElementById("result").innerHTML = "You Lost";
       }
     break;
     case 'scissors':
       // window.alert("scissors selected");
-      if(computerChoice === 'rock')
-      {
-          // document.getElementById("result").innerHTML = "You Lost";
-      }
-      else if(computerChoice === 'paper')
-      {
-        // document.getElementById("result").innerHTML = "You Won";
-      }
-      else
       {
         // document.getElementById("result").innerHTML = "It's a Tie";
       }
